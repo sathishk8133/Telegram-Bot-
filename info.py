d28de2f322a00a4cd5128aea5242f16853040d25
sathishk8133 <84281626+sathishk8133@users.noreply.github.com>
2023-10-21 14:44:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,10 +5,10 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+USER_SESSION = environ.get('USER_SESSION', 'Mrtelegramaprbot')
+API_ID = int(environ['21102617'])
+API_HASH = environ['31de0d24a6b8048c48730bc420f4b70c']
+BOT_TOKEN = environ['6678342781:AAGLH8UWZ7iBIbwII0mjMYE8ESNCTrg28t4']
 USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
 
 # Bot settings
@@ -16,16 +16,16 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['6300568870'].split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['1001963036511'].split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 
 # MongoDB information
-DATABASE_URI = environ['DATABASE_URI']
-DATABASE_NAME = environ['DATABASE_NAME']
+DATABASE_URI = environ['mongodb+srv://ammbappurvillage:<Sathishk33>@cluster.wknjb7o.mongodb.net/?retryWrites=true&w=majority']
+DATABASE_NAME = environ['ammbappur']
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
